ef04a719e9b1d39afbbe5fa9218a6bdda1e02044
congnguyen <cong.nguyen@huuk.social>
2023-01-11 14:28:13 +0700

    Add integrationg for controller

--- src/controllers/fixtures/fixtures.controller.spec.ts
+++ src/controllers/fixtures/fixtures.controller.spec.ts
@@ -1,5 +1,6 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { FixturesController } from './fixtures.controller';
+import { raw } from 'express';
 
 describe('FixturesController', () => {
   let controller: FixturesController;
@@ -14,5 +15,11 @@ describe('FixturesController', () => {
 
   it('should be defined', () => {
     expect(controller).toBeDefined();
+  });
+  it('should test interation', async () => {
+    const from = '2023-01-09T14:00:00+00:00';
+    const to = '2023-01-10T14:00:00+00:00';
+    const [result, count] = await controller.filterByCondition(from, to, 1, 10);
+    expect(count).toEqual(0);
   });
 });
